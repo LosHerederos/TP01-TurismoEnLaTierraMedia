2cbdcfe17abeda9e82efd38a11c3366a1ee19158
Pedro Rolando <pmrolando@gmail.com>
2021-08-25 22:49:56 -0300

    Agrega paquetes y sus clases principales

---
+++ src/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+import tierraMedia.SistemaDeSugerencias;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Bienvenidos!");
+		
+		SistemaDeSugerencias sistema = new SistemaDeSugerencias();
+	}
+
+}
---
+++ src/tierraMedia/SistemaDeSugerencias.java
@@ -0,0 +1,12 @@
+package tierraMedia;
+
+public class SistemaDeSugerencias {
+
+	String mensaje = "A la Tierra Media!";
+
+	public SistemaDeSugerencias() {
+		System.out.println(mensaje);
+	}
+	
+	
+}
