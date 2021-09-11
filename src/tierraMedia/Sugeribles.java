eaba82ef327aab450a49137430ae5dd6efd9b457
Pedro Rolando <pmrolando@gmail.com>
2021-09-11 17:10:02 -0300

    Actualiza interfaz Sugeribles

--- src/tierraMedia/Sugeribles.java
+++ src/tierraMedia/Sugeribles.java
@@ -1,43 +1,12 @@
 package tierraMedia;
 
-
-/*
- * 
- * Revisar
 public interface Sugeribles {
-	
-	public static int costo=0;
-	
-	public static double Tiempo=0;	
-		
 	
 	public int getCosto();
-	
-	public double getTiempo();
-	
-	public TipoDeAtracciones getTipoDeAtraccion();
-*/
-	
-
-
-
-public interface Sugeribles {
-	//public boolean esCupoCompleto(); 
-	//public void agregarVisitante(); 
-	abstract public int getCosto(); 
-	//public void setCostoVisita(int costoVisita); 
-	abstract public double getTiempo();
-	//public void setTiempoParaRealizarla(double tiempoParaRealizarla);
-	//public int getCupoPersonas();
-	//public void setCupoPersonas(int cupoPersonas); 
-	//public int getVisitantes();
-	//public void setVisitantes(int visitantes);
-    //public String getNombre();
-    //public void setNombre(String nombre);
-	abstract TipoDeAtracciones getTipoDeAtraccion();
-	//abstract void setTipoDeAtraccion(TipoDeAtracciones tipoDeAtraccion);	
-	//TipoDeAtracciones getTipoDeAtracciones();
-	//void setTipodeAtraccion(TipoDeAtracciones tipoDeAtraccion);
+    public double getTiempo();
+    public void agregarVisitante();
+    public boolean esCupoCompleto();
+    TipoDeAtracciones getTipoDeAtraccion();
     
 }
 
