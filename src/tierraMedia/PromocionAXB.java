b996c5beac6e811798b0202c3f156f17b80f15cd
JavierBrugo <javsbrgo@gmail.com>
2021-09-14 19:42:18 -0300

    PromocionesAXB

--- src/tierraMedia/PromocionAXB.java
+++ src/tierraMedia/PromocionAXB.java
@@ -1,6 +1,7 @@
 package tierraMedia;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class PromocionAXB extends Promocion {
@@ -12,21 +13,15 @@ public class PromocionAXB extends Promocion {
 
 	}
 
-	//public String getAtraccionesPagas() {
 	public List<Atraccion> getAtraccionesPagas() {
-//		String nombreAtraccion = "";
-//		for (Atraccion atracciones : atraccionesPagas) {
-//			nombreAtraccion = atracciones.getNombre();
-//		}
-//		return (nombreAtraccion);
 		return this.atraccionesPagas;
 	}
 
 	@Override
 	public int getCosto() {
 		int costoTotal = 0;
-		for (Atraccion atracciones : atraccionesPagas) {
-			costoTotal += atracciones.getCosto();
+		for (Atraccion atraccion : atraccionesPagas) {
+			costoTotal += atraccion.getCosto();
 		}
 		return (costoTotal);
 	}
@@ -35,6 +30,36 @@ public class PromocionAXB extends Promocion {
 		this.atraccionesPagas.addAll(atraccionesPagas);
 	}
 
+	@Override
+	public double getTiempo() {
+		double tiempoTotal = 0;
+		for (Atraccion atraccion : atraccionesPagas) {
+			tiempoTotal += atraccion.getTiempo();
+		}
+		tiempoTotal += super.getTiempo();
+		return (tiempoTotal);
+	}
+	@Override
+	public void agregarVisitante() {
+		if (!this.esCupoCompleto()) {
+			for (Atraccion atraccion : atraccionesPagas) {
+				atraccion.agregarVisitante();
+			}
+
+		}
+	}
+	@Override
+	public boolean esCupoCompleto() {
+		boolean cupoCompleto = false;
+
+		Iterator<Atraccion> atraccion = atraccionesPagas.listIterator();
+		while (atraccion.hasNext()) {
+			if (atraccion.next().esCupoCompleto()) {
+				cupoCompleto = true;
+			}
+		}
+		return cupoCompleto;
+	}
 	@Override
 	public String reduccionCostoTotal() {
 		return (super.getNombre() + ":" + "Comprando " + getAtraccionesPagas() + super.getAtraccion() + "gratis");
