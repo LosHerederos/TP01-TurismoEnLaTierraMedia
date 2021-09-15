9d66f76fd55929e2a2db9c5afcd3e01372a057d0
Pedro Rolando <pmrolando@gmail.com>
2021-09-15 19:59:50 -0300

    Modifica PromocionTest y AtraccionTest

--- test/tierraMedia/AtraccionTest.java
+++ test/tierraMedia/AtraccionTest.java
@@ -9,6 +9,13 @@ public class AtraccionTest {
 	@Test
 	public void test() {
 		Atraccion atraccion = new Atraccion(25, 2.5, 10, TipoDeAtracciones.AVENTURA, "Una aventura como ninguna");
+		assertEquals(25, atraccion.getCosto());
+		assertEquals(2.5, atraccion.getTiempo(), 0.001);
+		assertEquals(10, atraccion.getCupoPersonas());
+		assertEquals(TipoDeAtracciones.AVENTURA, atraccion.getTipoDeAtraccion());
+		assertEquals(0, atraccion.getVisitantes());
+		atraccion.agregarVisitante();
+		assertEquals(1, atraccion.getVisitantes());
 	}
 
 }
--- test/tierraMedia/UsuarioTest.java
+++ test/tierraMedia/UsuarioTest.java
@@ -11,9 +11,6 @@ public class UsuarioTest {
 		Usuario usuario = new Usuario("Pedro", 100, 10.0, TipoDeAtracciones.AVENTURA);
 		Atraccion atraccion = new Atraccion(25, 2.5, 10, TipoDeAtracciones.AVENTURA, "Una aventura como ninguna");
 		usuario.aceptarSugerencia(atraccion);
-		
-		
-		
 	}
 
 }
